860ff2146fac6aba68609166c4e207df32590938
Pratik <pratikkhatri520@gmail.com>
2024-09-30 20:41:33 +0530

    added home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="h-screen flex justify-center items-center">
+        <div className="">Welcome Admin Panel</div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
