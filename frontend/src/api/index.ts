7fb538b793fe2e798ea2da13158da42bcfd3f0d0
Pratik <pratikkhatri520@gmail.com>
2024-09-30 20:40:29 +0530

    index file

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,10 @@
+export const SERVER_BASE_URL = `http://localhost:3000/api/v1`;
+
+export const API = {
+  users: {
+    login: `${SERVER_BASE_URL}/login`,
+  },
+  employee: {
+    fetchEmployeeList: `${SERVER_BASE_URL}/employee-list`,
+  },
+};
