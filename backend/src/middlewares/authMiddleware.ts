26b197d1a6db1d3607284387ab661af9eda9b2c6
Pratik <pratikkhatri520@gmail.com>
2024-09-30 11:42:58 +0530

    added auth middleware

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel"; 
+
+export const authUser = async (req: any, res: Response, next: NextFunction) => {
+  const token = req.header("Authorization")?.split(" ")[1]; 
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "No token provided, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+      userId: string;
+    };
+    console.log(decoded);
+
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = { _id: user._id };
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token is not valid" });
+  }
+};
