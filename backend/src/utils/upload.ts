e434e1932675d232cfc92a14ba8bb97b465fe086
Pratik <pratikkhatri520@gmail.com>
2024-09-30 11:43:16 +0530

    added image upload

---
+++ backend/src/utils/upload.ts
@@ -0,0 +1,28 @@
+import multer, { FileFilterCallback } from 'multer';
+import path from 'path';
+import { Request } from 'express';
+
+const storage = multer.diskStorage({
+    destination: (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
+        cb(null, 'uploads/'); 
+    },
+    filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+    const ext = path.extname(file.originalname).toLowerCase(); 
+    if (ext !== '.jpg' && ext !== '.png') {
+        return cb(new Error('Only jpg and png files are allowed!'));
+    }
+    cb(null, true);
+};
+
+const upload = multer({ 
+    storage, 
+    fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 }, 
+});
+
+export default upload;
